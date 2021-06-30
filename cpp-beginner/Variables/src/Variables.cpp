e2598115b9b43fe0aa50cd23f5af90e750f5ebf0
mak627 <m.khadka69@gmail.com>
2021-07-01 01:19:32 +0200

    Third commit - Variables

---
+++ cpp-beginner/Variables/src/Variables.cpp
@@ -0,0 +1,16 @@
+// Name        : Variables.cpp
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Starting variabes..." << endl;
+	int numStudents = 5;
+	cout << "The number of students is " << numStudents << endl;
+
+	cout << "New student has joined!!" << endl;
+	numStudents = numStudents + 1;
+	cout << "The new number of students is " << numStudents << endl;
+
+	return 0;
+}
